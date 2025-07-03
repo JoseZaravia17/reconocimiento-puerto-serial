3c18afba2d03499a0c58cfd82eac535c213dfd9f
JoseZaravia17 <123758341+JoseZaravia17@users.noreply.github.com>
2025-07-03 12:45:37 -0500

    configuracion inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,145 @@
-import Image from "next/image";
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import { useState } from "react";
 
-export default function Home() {
+// // Extiende la interfaz Navigator para incluir 'serial'
+// declare global {
+//   interface Navigator {
+//     serial: any;
+//   }
+// }
+
+// export default function SerialReader() {
+//   const [peso, setPeso] = useState("");
+
+//   const handleSerialConnect = async () => {
+//     try {
+//       // if (ports.length > 0) {
+//       //   console.log('ya autorizado');
+//       //   port = ports[0];
+//       // } else {
+//       //port = await navigator.serial.requestPort({ filters: [] });
+//       // }
+//       const port = await navigator.serial.requestPort();
+//       await port.open({ baudRate: 9600 });
+//       // await port.open({ baudRate: 1200 });
+
+//       const decoder = new TextDecoderStream();
+//       port.readable.pipeTo(decoder.writable);
+
+//       const inputStream = decoder.readable;
+//       console.log(inputStream);
+//       const reader = inputStream.getReader();
+//       console.log(reader);
+
+//       while (true) {
+//         console.log("BUCLE")
+//         const { value, done } = await reader.read();
+//         console.log("VERIFICACION", done)
+//         if (done) break;
+//         if (value) {
+//           console.log("Peso:", value);
+//           setPeso(value);
+//         }
+//       }
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   return (
+//     <div className="mt-44 text-center">
+//       {peso &&
+//         <div className="text-7xl font-bold tracking-tighter mb-4 w-fit h-auto mx-auto rounded-md border-2 border-dashed border-black">
+//           <p className="mb-4 mx-5">Peso: {peso}</p>
+//         </div>
+//       }
+//       <Button onClick={handleSerialConnect}>Comenzar Lectura de Balanza</Button>
+//     </div>
+//   );
+// }
+
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+declare global {
+  interface Navigator {
+    serial: any;
+  }
+}
+
+export default function SerialReader() {
+  const [peso, setPeso] = useState("");
+
+  const handleSerialConnect = async () => {
+    try {
+      const port = await navigator.serial.requestPort();
+      await port.open({ baudRate: 9600 });
+
+      const decoder = new TextDecoderStream();
+      port.readable.pipeTo(decoder.writable);
+      const inputStream = decoder.readable;
+      const reader = inputStream.getReader();
+
+      let buffer = "";
+
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) break;
+        if (!value) continue;
+        console.log(value)
+
+        for (let i = 0; i < value.length; i++) {
+          const char = value[i];
+
+          if (char === ",") {
+            // Fin 
+            const pesoFinal = procesarFrame(buffer);
+            if (pesoFinal) {
+              setPeso(pesoFinal);
+              console.log("Peso:", pesoFinal);
+            }
+            buffer = ""; 
+          } else {
+            buffer += char;
+          }
+        }
+      }
+
+      await reader.releaseLock();
+    } catch (err) {
+      console.error("Error de conexión serial:", err);
+    }
+  };
+
+  function procesarFrame(frame: string): string | null {
+    // Ejemplo frame: "0   823   000 ?"
+    const clean = frame.replace(/\s+/g, " ").trim(); 
+    // ["0", "823", "000", "?"]
+    const partes = clean.split(" "); 
+
+    const pesoRaw = partes.find(p => /^\d+$/.test(p) && p !== "000" && p !== "0");
+    if (!pesoRaw) return null;
+
+    const pesoFinal = parseInt(pesoRaw) / 100;
+    return pesoFinal.toFixed(2);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="mt-44 text-center">
+      {peso && (
+        <div className="text-7xl font-bold tracking-tighter mb-4 w-fit h-auto mx-auto rounded-md border-2 border-dashed border-black">
+          <p className="mb-4 mx-5">Peso: {peso} kg</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+      <Button onClick={handleSerialConnect}>
+        Comenzar Lectura de Balanza
+      </Button>
     </div>
   );
 }
